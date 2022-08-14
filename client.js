2b209f930ccf29f192b0154f3df76ea8f1b1f8ef
Matthew Davis <matc.davis@gmail.com>
2022-08-14 21:41:56 +0000

    rewrite the client prompts in a simple .write(), the readLine was causing a async timing issue

--- client.js
+++ client.js
@@ -12,33 +12,32 @@ const client = net.createConnection({
   port: 3000,
 });
 
+// rl.setPrompt("\nPlease enter a file path\n");
+
 client.setEncoding('utf8');
 
+
+//display all data coming from the server in utf8
 client.on("data", (data) => {
-  // console.log("\nServer says:", data);
-  console.log(data);
- 
-})
+  console.log(`${data}`);
+});
 
-// client.write("Hello from client!");
-rl.write("Hello, You have logged into an app that will allow you to access data from a given file on the server.\n")
 
+console.log("Hello, You have logged into an app that will allow you to access data from a given file on the server.\n");
+
+// display all the available files here
 //prompt user to enter a file path
-//I want to display all the available files here
-
-rl.setPrompt("\nPlease enter a file path\n")
-
-//keep asking for a file to be read unless the user exits the server.
-
-rl.prompt();
-
+//open a readline stream as soon the client enters the first file
+  
 rl.on("line", (input) => {
   client.write(input);
-  rl.prompt()
-}).on('close', () => {
-  console.log("\nYou have logged out\n")
+});
+
+rl.on('close', () => {
+  console.log("\nYou have logged out\n");
   process.exit();
-})
+});
+
 
 
 
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const net = require("net");
 const readline = require("readline");
 const fs = require("fs");
+const msg = "\nPlease enter a file path\n";
+
 
 
 const rl = readline.createInterface({
@@ -12,44 +14,56 @@ const rl = readline.createInterface({
 const server = net.createServer();
 
 server.listen(3000, () => {
-  console.log("Server on port 3000")
-})
+  console.log("Server on port 3000");
+});
+
 
 
 server.on("connection", (client) => {
   console.log("New client connected");
-  // client.write("\nHello from Server!")
-  client.setEncoding("utf-8")  
 
-//on receiving data from the client, display what that file path is to the client and the server and send that file path into readFile
+
+  //display to the client all the local files that can be read on a new connection
+  fs.readdir("./", "utf-8", (err, files) => {
+    if (err) {
+      console.log(err);
+    }
+    const toRead = files.join('\n');
+    client.write('Here are the available files:\n' + toRead + '\n' + msg);
+  });
+
+
+  //on receiving data from the client, display what that file path is to the client and the server and send that file path into readFile
   client.on("data", (data) => {
     console.log(`The file path entered is ${data}`);
     client.write(`The file path entered is ${data}`);
 
-    //receives a file path from the client and send the data back to the client 
+
+    //receives a file path from the client and send the data back to the client
     fs.readFile(data, (err, body) => {
       if (err) {
-        client.write(`There was an error: ${err}`)
+        client.write(`There was an error: ${err}`);
         return;
       }
-      client.write(`This file contains: " -->${ body }<--"`);
+      client.write(`This file contains: \nSTART\n${body}\n\tEND<-- ${msg}`);
       return;
-    })
+    });
 
- 
-  })
+  });
 
-  client.on("error",(error) => {
-    if (error.code === "ECONNRESET") console.log("A client has left")
-    else console.log(error)
-  })
+
+
+  client.on("error", (error) => {
+    if (error.code === "ECONNRESET") console.log("A client has left due to error", error.name);
+    else console.log(error);
+  });
 
   client.on("end", () => {
-    console.log("A client has left")
-  })
+    console.log("A client has left");
+  });
 
 
-})
+});
 
 
 
